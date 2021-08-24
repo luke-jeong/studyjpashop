c9eeee26e8f1fddd17b444950bc5eb113342d5bc
luke-jeong <luke@catious.io>
2021-08-24 16:47:14 +0900

    Complete to build Domain

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,15 +16,30 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="delivery_id")
     private Delivery delivery;
     private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING) // ordinal로 하면 숫자로 들어오므로 상태 추가 시 오류 발생 가능성 있음.
     private OrderStatus status; // 주문상태(order, cancel)
+
+    // 연관관계 메서드
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+
 }
